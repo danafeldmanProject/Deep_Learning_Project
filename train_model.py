23834f01a22a31491673c72824a0694bd6d54014
danafeldmanProject <86161854+danafeldmanProject@users.noreply.github.com>
2021-07-04 03:04:48 +0300

    Update train_model.py

--- train_model.py
+++ train_model.py
@@ -9,18 +9,21 @@ Created on Thu Jun 17 16:36:17 2021
 import os
 import cv2
 import numpy as np
-import matplotlib.pyplot as plt
 
-import my_Model
 import prints
 
 
 
     
-def Put_Into_Lists(train_directory_path,test_directory_path,CLASSES,IMAGE_SIZE):  
-    class_names_label = {class_name:i for i, class_name in enumerate(CLASSES)}
+def Put_Into_Lists(train_directory_path,test_directory_path,class_names,image_size): 
+    '''
+        puts the data and labels into arrays
+        resizes images
+        returns the arrays
+    '''
+    class_names_label = {class_name:i for i, class_name in enumerate(class_names)}
     print(class_names_label)
-    nb_classes = len(CLASSES)
+    nb_classes = len(class_names)
     
     datasets = [train_directory_path,test_directory_path]
     output = []
@@ -46,8 +49,7 @@ def Put_Into_Lists(train_directory_path,test_directory_path,CLASSES,IMAGE_SIZE):
                 
                 # Open and resize the img
                 image = cv2.imread(img_path)
-                #image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-                image = cv2.resize(image, IMAGE_SIZE) 
+                image = cv2.resize(image, image_size) 
                 
                 # Append the image and its corresponding label to the output
                 images.append(image)
@@ -64,6 +66,9 @@ def Put_Into_Lists(train_directory_path,test_directory_path,CLASSES,IMAGE_SIZE):
     
 
 def Train(model,train_images, train_labels):
+    '''
+        train the model with train data and labels
+    '''
     model.compile(optimizer='adam',loss='binary_crossentropy',metrics=['accuracy'])
     history = model.fit(train_images, train_labels, batch_size=64, epochs=7, validation_split = 0.4)
     model.save('model.h5')
@@ -74,6 +79,9 @@ def Train(model,train_images, train_labels):
 
 
 def test_model(model,test_images, test_labels):
+    '''
+        test the model with test data and labels
+    '''
     test_loss = model.evaluate(test_images, test_labels)    
     predictions = model.predict(test_images)     # Vector of probabilities
     pred_labels = np.argmax(predictions, axis = 1) # We take the highest probability
@@ -81,13 +89,16 @@ def test_model(model,test_images, test_labels):
 
 def handle_train(model,train_images, train_labels):
     """
-    this public method manage the train section
+    manage the train section
     return history
     """
     history = Train(model,train_images, train_labels)
     prints.plot_accuracy_loss(history)
     return history
 
+    
+    
+    
     
     
     
